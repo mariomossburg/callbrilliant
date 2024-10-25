1b20c9c35d560e9e6fcb8d6659ed47e7da091848
mariano mossburg <marmossburg@gmail.com>
2024-10-25 16:14:59 -0300

    ...

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,16 +1,16 @@
-import markdownStyles from "./markdown-styles.module.css";
+// import markdownStyles from "./markdown-styles.module.css";
 
-type Props = {
-  content: string;
-};
+// type Props = {
+//   content: string;
+// };
 
-export function PostBody({ content }: Props) {
-  return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
-  );
-}
+// export function PostBody({ content }: Props) {
+//   return (
+//     <div className="max-w-2xl mx-auto">
+//       <div
+//         className={markdownStyles["markdown"]}
+//         dangerouslySetInnerHTML={{ __html: content }}
+//       />
+//     </div>
+//   );
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import Container from "@/app/_components/container";
-import { HeroPost } from "@/app/_components/hero-post";
-import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
-import { getAllPosts } from "@/lib/api";
+// import Container from "@/app/_components/container";
+// import { HeroPost } from "@/app/_components/hero-post";
+// import { Intro } from "@/app/_components/intro";
+// import { MoreStories } from "@/app/_components/more-stories";
+// import { getAllPosts } from "@/lib/api";
 import Hero from "./_components/Hero";
 
 export default function Index() {
-  const allPosts = getAllPosts();
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  // const allPosts = getAllPosts();
+  // const heroPost = allPosts[0];
+  // const morePosts = allPosts.slice(1);
 
   return (
     <main>
