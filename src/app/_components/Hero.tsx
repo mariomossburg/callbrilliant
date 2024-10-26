e73fb86907592952643e81136434280c1d2d8176
mariano mossburg <marmossburg@gmail.com>
2024-10-26 20:01:14 -0300

    menu responsiveness

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
             <div className="hero-overlay bg-opacity-60 rounded-xl"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <h1 className="mb-5 text-5xl font-bold">After 70 years, we've made it easy for you. </h1>
                     <p className="mb-5">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
